85d1a6a6a09c3a9c56d3508f704646ea72edbc61
Funmi7 <talabifunmilayo@gmail.com>
2019-10-31 09:09:15 +0100

    Added all the required endpoints

--- users/userRouter.js
+++ users/userRouter.js
@@ -6,38 +6,83 @@ const router = express.Router();
 
 router.post('/', validateUser, (req, res) => {
   userDb.insert(req.body)
-  .then(user => {
-    res.status(201).json(user);
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: `Error adding the user ${error.message}`
+      });
+    });
+});
+
+router.post('/:id/posts', [validateUserId, validatePost], (req, res) => {
+  const postInfo = { ...req.body, user_id: req.params.id }
+  postDb.insert(postInfo)
+    .then(post => {
+      res.status(210).json(post)
+    })
+    .then(error => {
+      res.status(500).json({
+        error: `Error adding post ${error.message}`
+      })
+    })
+})
+
+router.get('/', (req, res) => {
+  userDb.get(req.body)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: `Error fetching users ${error.message}`
+      });
+    });
+});
+
+
+router.get('/:id', validateUserId, (req, res) => {
+  res.json(req.user)
+});
+
+router.get('/:id/posts', validateUserId, (req, res) => {
+
+  userDb.getUserPosts(req.user.id)
+  .then(posts => {
+    res.status(200).json(posts);
   })
   .catch(error => {
     res.status(500).json({
-      error: `Error adding the user ${error.message}`
+      error: `Error fetching posts ${error.message}`
     });
   });
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.delete('/:id', validateUserId, (req, res) => {
+  userDb.remove(req.user.id)
+  .then(() => {
+    res.status(200).json({
+      message: "This user has been deleted"
+    })
+  })
+  .catch(error => {
+    res.status(500).json({
+      error: `Error removing the user ${error.message}`
+    })
+  })
 });
 
-router.get('/', (req, res) => {
-
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
+router.put('/:id', [validateUserId, validateUser], (req, res) => {
+  userDb.update(req.user.id, req.body)
+  .then(updatedUser => {
+    res.status(200).json(updatedUser)
+  })
+  .catch(error => {
+    res.status(500).json({
+      error: `Error updating user ${error}`
+    });
+  });
 });
 
 //custom middleware
@@ -62,33 +107,33 @@ function validateUserId(req, res, next) {
 };
 
 function validateUser(req, res, next) {
- if(Object.keys(req.body).length) {
-   next();
- } else if (!req.body.name) {
-   res.status(400).json({
-    message: "missing required name field"
-   })
- }
- else {
-   res.status(400).json({
-    message: "missing user data"
-   })
- }
+  if (Object.keys(req.body).length) {
+    next();
+  } else if (!req.body.name) {
+    res.status(400).json({
+      message: "missing required name field"
+    })
+  }
+  else {
+    res.status(400).json({
+      message: "missing user data"
+    })
+  }
 
 };
 
 function validatePost(req, res, next) {
-if(!Object.keys(req.body).length) {
-  res.status(400).json({
-    message: "missing post data"
-  })
-} else if (!req.body.text) {
-  res.status(400).json({
-    message: "missing required text field"
-  })
-} else {
-  next();
-}
+  if (!Object.keys(req.body).length) {
+    res.status(400).json({
+      message: "missing post data"
+    })
+  } else if (!req.body.text) {
+    res.status(400).json({
+      message: "missing required text field"
+    })
+  } else {
+    next();
+  }
 };
 
 module.exports = router;
